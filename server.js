badc800d085ff9276a0a1f7f269cd7c27edaf0ef
Connie Tran <connietrandev@gmail.com>
2020-07-11 17:59:41 -0400

    Remove unused dependencies for deploy

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const errorHandler = require('./lib/error_handler');
-const requestLogger = require('./lib/request_logger');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Log each request as it comes in for debugging
-app.use(requestLogger);
-// Register error handling middleware
-app.use(errorHandler);
-
 // run API on designated port (4741 in this case)
-app.listen(port, () => {
-  console.log('Listening on: http://localhost:' + port)
-})
+app.listen(PORT, () => {
+  console.log('Listening on: http://localhost:' + PORT);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Surviral API");
+});
 
 module.exports = app;
