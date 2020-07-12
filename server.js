3535949e7c04976168ae69d359eee5fda38da7c0
Connie Tran <connietrandev@gmail.com>
2020-07-12 09:09:22 -0400

    Add user helper functions

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInGame } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
---
+++ users.js
@@ -0,0 +1,34 @@
+const users = [];
+
+// id from socket instance
+const addUser = ({ id, name, game }) => {
+    name = name.trim().toLowerCase();
+    game = game.trim().toLowerCase();
+
+    // If same user is trying to sign up for same game, return error 
+    const existingUser = users.find((user) => user.game === game && user.name === name);
+
+    if(existingUser) {
+        return { error: 'Username is taken' }
+    }
+
+    const user = { id, name, game };
+    users.push(user);
+
+    return { user }
+};
+
+const removeUser = (id) => {
+    // If user matches user's index, remove user from array
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1) return users.splice(index, 1)[0];
+};
+
+// If user matches on id, we know user exists
+const getUser = (id) => users.find((user) => user.id === id);
+
+// Get all users in Game
+const getUsersInGame = (game) => users.filter((user) => user.game === game);
+
+module.exports = { addUser, removeUser, getUser, getUsersInGame };
